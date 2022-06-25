2bb28187b73e1d28c5fa0a51862957f062072f61
Naomi Ahmed <yretenai@gmail.com>
2022-06-25 16:51:42 +0100

    Support --raw

--- Cuddle.Headless/Program.cs
+++ Cuddle.Headless/Program.cs
@@ -15,6 +15,7 @@ public static class Program {
     public static void Main() {
         var flags = CommandLineFlagsParser.ParseFlags<CuddleFlags>();
         if (flags == null) {
+            CommandLineFlagsParser.PrintHelp<CuddleFlags>(CommandLineFlagsParser.PrintHelp, true);
             return;
         }
 
@@ -35,23 +36,40 @@ public static class Program {
 
                 var result = Path.Combine(flags.OutputPath, file.MountedPath[0] == '/' ? file.MountedPath[1..] : file.MountedPath);
                 var ext = Path.GetExtension(file.MountedPath).ToLower();
-                if (ext == ".uasset") {
-                    using var asset = file.ReadAsset();
-                    if (asset == null) {
+                if (ext is ".uasset" or ".umap") {
+                    try {
+                        using var asset = file.ReadAsset();
+                        if (asset == null) {
+                            continue;
+                        }
+
+                        Log.Information("{Path} -> uasset", file.MountedPath);
+
+                        if (!flags.NoJSON) {
+                            var objects = asset.GetExports();
+                            var json = JsonSerializer.Serialize(objects, JsonSettings.Options);
+                            result.EnsureDirectoryExists();
+                            File.WriteAllText(Path.ChangeExtension(result, ".json"), json);
+                        }
+                    } catch (Exception e) {
+                        Log.Error(e, "Failed to process uasset {Path}", file.MountedPath);
+                    }
+                }
+
+                if (flags.Raw) {
+                    using var data = file.ReadFile();
+                    if (data.Length == 0) {
                         continue;
                     }
 
-                    Console.WriteLine(file.MountedPath);
-
-                    if (!flags.NoJSON) {
-                        var objects = asset.GetExports();
-                        var json = JsonSerializer.Serialize(objects, JsonSettings.Options);
-                        result.EnsureDirectoryExists();
-                        File.WriteAllText(Path.ChangeExtension(result, ".json"), json);
-                    }
+                    Log.Information("{Path} -> Raw", file.MountedPath);
+                    result.EnsureDirectoryExists();
+                    using var stream = File.OpenWrite(result);
+                    stream.SetLength(0);
+                    stream.Write(data.Span);
                 }
             } catch (Exception e) {
-                Log.Error(e, "Failure deserializing {Path}", file.ObjectPath);
+                Log.Error(e, "Failure processing {Path}", file.ObjectPath);
             }
         }
     }
