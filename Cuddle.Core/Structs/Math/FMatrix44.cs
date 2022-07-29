a79580a94ca2a09da7aafda0a9aec1f95ae24f76
Naomi <yretenai@gmail.com>
2022-07-29 07:35:52 +0100

    Update FMatrix44.cs

--- Cuddle.Core/Structs/Math/FMatrix44.cs
+++ Cuddle.Core/Structs/Math/FMatrix44.cs
@@ -13,15 +13,15 @@ public record FMatrix44 : FFallbackStruct {
     public FMatrix44(FArchiveReader reader, string name) {
         var isDouble = name == "Matrix44" && reader.VersionUE5 >= EObjectVersionUE5.LARGE_WORLD_COORDINATES || name == "Matrix44d";
         var planeName = isDouble ? "Plane4d" : "Plane4f";
-        XPlane = new FVector4(reader, planeName);
-        YPlane = new FVector4(reader, planeName);
-        ZPlane = new FVector4(reader, planeName);
-        WPlane = new FVector4(reader, planeName);
+        XPlane = new FPlane(reader, planeName);
+        YPlane = new FPlane(reader, planeName);
+        ZPlane = new FPlane(reader, planeName);
+        WPlane = new FPlane(reader, planeName);
     }
 
-    public FVector4 XPlane { get; set; }
-    public FVector4 YPlane { get; set; }
-    public FVector4 ZPlane { get; set; }
-    public FVector4 WPlane { get; set; }
+    public FPlane XPlane { get; set; }
+    public FPlane YPlane { get; set; }
+    public FPlane ZPlane { get; set; }
+    public FPlane WPlane { get; set; }
     public Matrix4X4<double> ToSilk() => new(XPlane.ToSilk(), YPlane.ToSilk(), ZPlane.ToSilk(), WPlane.ToSilk());
 }
